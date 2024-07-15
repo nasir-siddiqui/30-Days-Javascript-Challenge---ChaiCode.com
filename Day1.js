d84b122548be75a09023a4d40def7cdedb84a2fb
Nasir <131708693+nasir-siddiqui@users.noreply.github.com>
2024-07-15 22:25:46 +0530

    Update and rename Day 1 to Day1.js

---
+++ Day1.js
@@ -0,0 +1,39 @@
+//Task 1:
+var num = 10;
+console.log(num);
+
+//Task 2:
+let str = "Hello, World";
+console.log(str);
+
+//Task 3:
+const isTrue = true;
+console.log(isTrue);
+
+//Task 4:
+let number = 20;
+let string = "I am String";
+let bools = false;
+let food = {
+          fruit: "Apple",
+          qty: 2
+          };
+let array = ["abc",123,true];
+
+console.log(typeof number);
+console.log(typeof string);
+console.log(typeof bools);
+console.log(typeof food);
+console.log(typeof array);
+
+//Task 5
+let value = 5;
+console.log("Initial value:"+value);
+value = 10;
+console.log("New Value:"+value);
+
+//Task 6
+const task6 = "hello";
+console.log(task6)
+task6 = 5;
+// TypeError: Assignment to constant variable.
