2ac6110f1683a354e6e69e542b4a2f35d3ed0574
Nasir <131708693+nasir-siddiqui@users.noreply.github.com>
2024-07-16 20:32:36 +0530

    Create Day2.js

---
+++ Day2.js
@@ -0,0 +1,53 @@
+// Activity 1 Arithematic Operation
+let num1 = 5;
+let num2 = 10;
+
+//Task 1
+console.log(num1 + num2);
+//Task 2
+console.log(num - num2);
+//Task 3
+console.log(num * num2);
+//Task 4
+console.log(num1 / num2);
+//Task 5
+console.log(num % num2);
+
+//Activity 2 Assignment Operation
+//Task 6
+num1+=5;
+console.log(num1);
+//Task 7
+num1-=5;
+console.log(num1);
+
+//Activity 3 Comparison Operation
+//Task 8
+console.log(10 > 5);
+console.log(10 < 5);
+
+//Task 9
+console.log(10 >= 20);
+console.log(10 <= 20);
+
+//Task 10
+console.log( 10 == 20);
+console.log( 10 === 10);
+
+//Activity 4 Logical Operation
+//Task 11
+let x = 10 ;
+let y = 20;
+console.log(x == 5 && y == 20)
+
+//Task 12
+let x = 10 ;
+let y = 20;
+console.log(x == 5 || y == 20)
+
+//Task 13
+console.log( 10 !== 2);
+
+//Activity 5 Ternary Operation
+//Task 14
+console.log(20 >= 0 ? "Positive" : "Negative");
