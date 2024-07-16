ffa5149224ad41d9a882120cbf1651b8f314b3e5
Nasir <131708693+nasir-siddiqui@users.noreply.github.com>
2024-07-16 20:52:38 +0530

    Create Day3.js

---
+++ Day3.js
@@ -0,0 +1,33 @@
+//Activity 1
+//Task 1
+let num1 = 10;
+if(num1 > 0){
+  console.log("Positive");
+}
+else if(num1<0){
+  console.log("Negative");
+}
+else{
+  console.log("Zero");
+}
+
+//Task 2
+let age = 20;
+if(age>= 18){
+  console.log("Eligible");
+}
+else{
+  console.log("Not Eligible");
+}
+
+//Task 3
+let num1 = 10,num2 =5, num3 =30;
+if(num1 > num2 && num1 > num3){
+        console.log(`${num1} is the Largest number.`);
+    }
+else if(num2 > num3){
+        console.log(`${num2} is the Largest number`);
+    }
+else{
+        console.log(`${num3} is the Largest number`);
+    }
